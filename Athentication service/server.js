fcdd4dc908f79fb119fd0aff03ee6a8bafc1640d
uplandwave <113386040+uplandwave@users.noreply.github.com>
2022-09-28 13:47:34 -0600

    new class

--- Athentication service/server.js
+++ Athentication service/server.js
@@ -3,9 +3,21 @@ const bodyParser = require("body-parser");
 const {v4 : uuidv4} = require("uuid");
 const port = 3000;
 const app = express();
-
-app.use(bodyParser.json());
-
+const {createClient} = require("redis")
+const md5 = require("md5")
+const redisClient= createClient(
+    {
+        url:"redis:default@localhost:6379",
+    }
+    )
+    
+    // app.listen(port, async()=>{
+    //     await redisClient.connect();
+    //     console.log("Listening on port: ",port);
+    // })
+    
+    app.use(bodyParser.json());
+    
 app.listen(port, async ()=>{
     console.log("Listening on port "+port);
 })
@@ -31,5 +43,4 @@ app.post("/login", (req,res)=>{
     }
 
 })
-
 
